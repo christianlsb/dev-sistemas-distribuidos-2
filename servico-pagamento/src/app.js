00bc88e5e3dba75dc987fae2cd9b6cf6d1423c78
Christian Lisboa (Tiki) <christian.moreno@tikiweb.com.br>
2024-10-26 12:53:21 -0300

    Refactorizar código para remover rota de hello world

--- servico-pagamento/src/app.js
+++ servico-pagamento/src/app.js
@@ -5,10 +5,6 @@ const app = express();
 app.use(express.json());
 
 app.post('/pagar', paymentService.handlePaymentRequest);
-//hello world
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
